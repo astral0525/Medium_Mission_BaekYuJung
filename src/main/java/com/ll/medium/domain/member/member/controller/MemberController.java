227c1c1673dbb92987bd22db64db5248c1706b1c
astral0525 <astral0525@gmail.com>
2023-12-13 21:43:12 +0900

    member id

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.medium.domain.member.member.controller;
 
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -34,9 +35,11 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String signup(@Valid JoinForm joinForm) {
-        memberService.join(joinForm.getUsername(), joinForm.getPassword());
-        return "redirect:/";
+    public String join(@Valid JoinForm joinForm) {
+        Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
+        long id = member.getId();
+
+        return "redirect:/?msg=No %d member joined.".formatted(id);
     }
 
 
