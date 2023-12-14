d34103ecfd6d43ce5624b810b7fcb78b38f206a4
astral0525 <astral0525@gmail.com>
2023-12-14 09:31:56 +0900

    toast alert

--- src/main/resources/static/resource/global/global.js
+++ src/main/resources/static/resource/global/global.js
@@ -1,4 +1,61 @@
+/* 유틸, 토스틀 */
+toastr.options = {
+    closeButton: true,
+    debug: false,
+    newestOnTop: true,
+    progressBar: true,
+    positionClass: "toast-top-right",
+    preventDuplicates: false,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    timeOut: "5000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut"
+};
+
+function parseMsg(msg) {
+    const [pureMsg, ttl] = msg.split(";ttl=");
+
+    const currentJsUnixTimestamp = new Date().getTime();
+
+    if (ttl && parseInt(ttl) < currentJsUnixTimestamp) {
+        return [pureMsg, false];
+    }
+
+    return [pureMsg, true];
+}
+
+function toastMsg(isNotice, msg) {
+    if (isNotice) toastNotice(msg);
+    else toastWarning(msg);
+}
+
+function toastNotice(msg) {
+    const [pureMsg, needToShow] = parseMsg(msg);
+
+    if (needToShow) {
+        toastr["success"](pureMsg, "알림");
+    }
+}
+
+function toastWarning(msg) {
+    const [pureMsg, needToShow] = parseMsg(msg);
+
+    if (needToShow) {
+        toastr["warning"](pureMsg, "경고");
+    }
+}
+
+
+
+
+/*유틸 URL*/
 function getUrlParams(urlString){
+
     const url = new URL(urlString);
     const queryParams = new URLSearchParams(url.search);
 
