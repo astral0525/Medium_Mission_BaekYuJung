2e575c26caf82cdac17c0c72d8d8d9dbe30f3307
astral0525 <astral0525@gmail.com>
2023-12-14 10:12:47 +0900

    Rq

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -2,21 +2,27 @@ package com.ll.medium.domain.member.member.controller;
 
 import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
+import com.ll.medium.global.rq.Rq.Rq;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.apache.catalina.util.URLEncoder;
+import org.codehaus.groovy.ast.ClassNode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
     @GetMapping("/join")
     public String showJoin(){
@@ -39,8 +45,10 @@ public class MemberController {
         Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
         long id = member.getId();
 
-        return "redirect:/?msg=No %d member joined.".formatted(id);
+        return rq.redirect(
+                "/",
+                "%s님 환영합니다. 회원가입이 완료되었습니다. 로그인 후 이용해주세요.".formatted(member.getUsername())
+        );
     }
-
 
 }
