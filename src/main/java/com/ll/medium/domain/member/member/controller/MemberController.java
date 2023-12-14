7da26791704bb4a536cbe083c1d2e3b18c9da904
astral0525 <astral0525@gmail.com>
2023-12-14 12:15:50 +0900

    RsData

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ll.medium.domain.member.member.controller;
 import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
 import com.ll.medium.global.rq.Rq.Rq;
+import com.ll.medium.global.rsData.RsData.RsData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -21,32 +22,30 @@ public class MemberController {
     private final Rq rq;
 
     @GetMapping("/join")
-    public String showJoin(){
+    public String showJoin() {
         return "domain/member/member/join";
     }
 
-    @Getter
     @Setter
-    public static class JoinForm{
+    @Getter
+    public static class JoinForm {
         @NotBlank
         private String username;
         @NotBlank
         private String password;
-
-
     }
 
     @PostMapping("/join")
     public String join(@Valid JoinForm joinForm) {
-        Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
-        if (member ==null){
-            return rq.historyBack( "이미 존재하는 회원입니다.");
+        RsData<Member> joinRs = memberService.join(joinForm.getUsername(), joinForm.getPassword());
+
+        if (joinRs.isFail()) {
+            return rq.historyBack(joinRs.getMsg());
         }
 
         return rq.redirect(
                 "/",
-                "%s님 환영합니다. 회원가입이 완료되었습니다. 로그인 후 이용해주세요.".formatted(member.getUsername())
+                joinRs.getMsg()
         );
     }
-
 }
